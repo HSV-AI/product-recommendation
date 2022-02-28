d729af12973fd1d84b7e67646b84687a01d46da6
J. Langley <jameselangley@gmail.com>
2022-02-27 23:44:44 -0600

    Added confusion matrix scoring

--- src/productrec/pipelines/scoring/nodes.py
+++ src/productrec/pipelines/scoring/nodes.py
@@ -3,11 +3,52 @@ from typing import Any, Dict, List
 import pandas as pd
 import numpy as np
 import implicit
+import random
 import scipy
 from sklearn import metrics
 import logging
 from scipy.sparse import coo_matrix, csr_matrix
 from tqdm.auto import tqdm
+
+def mask_matrix(ratings, pct_test = 0.2, seed = 42):
+    '''
+    This function will take in the original user-item matrix and "mask" a percentage of the original ratings where a
+    user-item interaction has taken place for use as a test set. The test set will contain all of the original ratings, 
+    while the training set replaces the specified percentage of them with a zero in the original ratings matrix. 
+    
+    parameters: 
+    
+    ratings - the original ratings matrix from which you want to generate a train/test set. Test is just a complete
+    copy of the original set. This is in the form of a sparse csr_matrix. 
+    
+    pct_test - The percentage of user-item interactions where an interaction took place that you want to mask in the 
+    training set for later comparison to the test set, which contains all of the original ratings. 
+    
+    returns:
+    
+    training_set - The altered version of the original data with a certain percentage of the user-item pairs 
+    that originally had interaction set back to zero.
+    
+    test_set - A copy of the original ratings matrix, unaltered, so it can be used to see how the rank order 
+    compares with the actual interactions.
+    
+    user_inds - From the randomly selected user-item indices, which user rows were altered in the training data.
+    This will be necessary later when evaluating the performance via AUC.
+    '''
+    test_set = ratings.copy() # Make a copy of the original set to be the test set. 
+    # test_set[test_set != 0] = 1 # Store the test set as a binary preference matrix
+    training_set = ratings.copy() # Make a copy of the original data we can alter as our training set. 
+    nonzero_inds = training_set.nonzero() # Find the indices in the ratings data where an interaction exists
+    nonzero_pairs = list(zip(nonzero_inds[0], nonzero_inds[1])) # Zip these pairs together of user,item index into list
+    
+    random.seed(seed) # Set the random seed to zero for reproducibility
+    num_samples = int(np.ceil(pct_test*len(nonzero_pairs))) # Round the number of samples needed to the nearest integer
+    samples = random.sample(nonzero_pairs, num_samples) # Sample a random number of user-item pairs without replacement
+    user_inds = [index[0] for index in samples] # Get the user row indices
+    item_inds = [index[1] for index in samples] # Get the item column indices
+    training_set[user_inds, item_inds] = 0 # Assign all of the randomly chosen user-item pairs to zero
+    training_set.eliminate_zeros() # Get rid of zeros in sparse array storage after update to save space
+    return training_set, test_set, list(set(user_inds)), list(set(item_inds)) # Output the unique list of user rows that were altered  
 
 def ranking_metrics_at_k(model, train_user_items, test_user_items, K=10, show_progress=True, num_threads=1):
     """ Calculates ranking metrics for a given trained model
@@ -153,6 +194,7 @@ def score_confusion(
     factors = params['factors']
     regularization = params['regularization']
     iterations = params['iterations']
+    seed = params['seed']
 
     log.info(params)
     log.info("Size of product_train: {}".format(product_train.shape))
@@ -164,35 +206,80 @@ def score_confusion(
                                         regularization=regularization,
                                         iterations=iterations, use_gpu=False)
 
-    start_new = len(user_vecs)
 
     model.user_factors = user_vecs
     model.item_factors = item_vecs
 
-    total = scipy.sparse.vstack((product_train,product_test))  # NOT np.vstack
-    popular_items = np.array(total.sum(axis = 0)).reshape(-1) # Get sum of item iteractions to find most popular
+    score = score_model(model, product_test, test_size=0.1)
+    log.info("Score: {}".format(score))
+    return score
+    
+def score_model(model, test_orders, test_size=0.1):
 
-    transactions = product_test.shape[0]
-    items = product_test.shape[1]
+    skipped = 0
+    
+    tplist = []
+    tnlist = []
+    fplist = []
+    fnlist = []
 
-    log.info(transactions)
-    log.info(items)
+    for row in test_orders:
 
-    # TODO - NEED TO FIND A BETTER WAY TO CALCULATE FOR NEW ORDERS
-    for transaction in range(transactions):
-        purchases = []
-        for i in range(product_test.indptr[1], product_test.indptr[2]):
-            purchases.append(product_test.indices[i])
+        TP = FP = FN = TN = 0
+
+        # Get the list of nonzero products
+        nonzero = row.nonzero()[1]
+        order_size = len(nonzero)
         
-        model.partial_fit_users([start_new], product_test.getrow(transaction))
-        start_new += 1
-                # model.recommend()
-    # for i in range(users):
-    #     log.info("Index: {} - Size: {}".format(i,product_test.indptr[i+1] - product_test.indptr[i]))
+        # Need to skip if there aren't multiple products
+        if order_size < 2:
+            skipped+=1
+            continue
 
+        temp = row.copy()
+        if test_size > 0:
+            mask_count = max(1, int(order_size * test_size))
+            mask_list = random.sample(list(nonzero), mask_count)
+            temp[np.zeros(mask_count), mask_list] = 0
+            temp.eliminate_zeros()
+
+        new_id = model.user_factors.shape[0]
+        model.partial_fit_users([new_id], temp.astype('double'))
+        recs, prob = model.recommend(new_id, temp, N=order_size, filter_already_liked_items=False)
+        for rec in recs:
+            if rec in nonzero:
+                TP+=1
+            else:
+                FP+=1
+
+        for product in nonzero:
+            if product not in recs:
+                FN+=1
+
+        TN = model.item_factors.shape[0] - TP - FP - FN
+        
+        tplist.append(TP)
+        tnlist.append(TN)
+        fplist.append(FP)
+        fnlist.append(FN)
+        
+    count = test_orders.shape[0] - skipped
+    
+    true_positive = np.mean(tplist)
+    false_positive = np.mean(fplist)
+    false_negative = np.mean(fnlist)
+    true_negative =  np.mean(tnlist)
+    
+    sensitivity = true_positive / (true_positive + false_negative)
+    specificity = true_negative / (true_negative + false_positive)
 
     return {
-        "precision": 1
+        'true_positive':true_positive,
+        'false_positive':false_positive,
+        'false_negative':false_negative,
+        'true_negative':true_negative,
+        'sensitivity':sensitivity,
+        'specificity':specificity
     }
 
 def score_auc(
@@ -221,7 +308,7 @@ def score_auc(
     model.user_factors = user_vecs
     model.item_factors = item_vecs
 
-    results = ranking_metrics_at_k(model, product_train, product_test, K=10, show_progress=True, num_threads=1)
+    results = score_model(model, product_test, test_size=0.1)
 
     log.info(results)
 
