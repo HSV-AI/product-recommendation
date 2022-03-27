f4ac850494a13f0016031f0e31690ab5273ac672
J. Langley <jameselangley@gmail.com>
2022-03-27 15:23:49 -0500

    Added reporting and mlxtend

--- src/productrec/pipelines/reporting/nodes.py
+++ src/productrec/pipelines/reporting/nodes.py
@@ -16,6 +16,19 @@ def report(transactions: pd.DataFrame, params: Dict) -> pd.DataFrame:
         config=params,
     )
 
+    # Log basic statistics for the transactions
+    order_count = len(pd.unique(transactions['order_id']))
+    customer_count = len(pd.unique(transactions['customer_id']))
+    product_count = len(pd.unique(transactions["product_id"]))
+    purchase_count = len(transactions)
+    wandb.log({
+        "order_count": order_count,
+        "customer_count": customer_count,
+        "product_count": product_count,
+        "purchase_count": purchase_count
+    })
+
+    # Log some graphs for customers
     user_counts = transactions \
         .drop_duplicates(['customer_id', 'order_id']) \
         .groupby('customer_id')['order_id'] \
@@ -29,6 +42,31 @@ def report(transactions: pd.DataFrame, params: Dict) -> pd.DataFrame:
     customer_table = wandb.Table(dataframe=user_df[:10])
     wandb.log({"top_10_customer_by_orders": customer_table})
 
+    plt.plot(user_counts) 
+    wandb.log({
+        "orders_by_customers_plot": plt
+    })
+
+    # Log some graphs for products
+    product_counts = transactions \
+        .drop_duplicates(['product_id', 'order_id']) \
+        .groupby('product_id')['order_id'] \
+        .count() \
+        .sort_values(ascending=False)
+
+    product_df = product_counts \
+        .reset_index() \
+        .rename(columns={"product_id":"Product", "order_id":"Orders"})
+
+    product_table = wandb.Table(dataframe=product_df[:10])
+    wandb.log({"top_10_product_by_orders": product_table})
+
+    plt.plot(product_counts) 
+    wandb.log({
+        "products_by_orders_plot": plt
+    })
+
+    # Break down users based on # orders vs total count of orders
     temp = (100. * user_counts / user_counts.sum()).sort_values(ascending=False)
 
     count = 0
@@ -62,6 +100,7 @@ def report(transactions: pd.DataFrame, params: Dict) -> pd.DataFrame:
     product_table = wandb.Table(dataframe=product_df[:10])
     wandb.log({"top_10_products_by_orders": product_table})
 
+    # Break down products based on # orders vs total count of orders
     temp = (100. * product_counts / product_counts.sum()).sort_values(ascending=False)
 
     count = 0
@@ -82,3 +121,4 @@ def report(transactions: pd.DataFrame, params: Dict) -> pd.DataFrame:
     plt.pie(buckets, labels=labels, autopct=lambda p: '{:.0f}'.format(p * len(temp) / 100))
     fig1.gca().set_title("Breakdown of products by order count")
     wandb.log({"product_breakdown": wandb.Image(plt)})
+
